2c68eb1d7a6dc172f4528b9f80d6468822249bbc
Rachel Huang <rachelhuang@Rachels-Laptop.local>
2025-02-21 13:43:08 -0500

    Consolidated data extraction requests from chatbot into one general instagram data download method. Will probably save hashtag data extraction method for parsing specific hashtags from captions of posts downloaded.

--- chatbot.py
+++ chatbot.py
@@ -5,7 +5,7 @@ from langchain_openai import ChatOpenAI
 from langchain.chains import LLMChain
 from prompts import social_media_assistant_prompt_template
 from dotenv import load_dotenv
-from extract_data import download_followers, download_hashtags, download_stories, download_saved
+from extract_data import download_general
 
 #setting environment variables
 load_dotenv()
@@ -16,22 +16,9 @@ async def query_llm():
     response = await cl.AskUserMessage(content = "what's your instagram username?", timeout = 120).send()
     user_name = ""
     if response is not None:
-        username = await (download_followers(response['output']))
-        text = f"thank you {user_name}. the accounts you follow have been downloaded successfully."
+        username = await (download_general(response['output']))
+        text = f"thank you {user_name}. your data has been downloaded successfully."
         await cl.Message(text).send()
-    response = await cl.AskUserMessage("Do you want to query data related to posts under specific hashtags? Enter [y/n].").send()
-    if response is not None and response['output'] in "yY":
-        hashtags = await cl.AskUserMessage("Please enter the hashtags that you want to query in a list.").send()
-        await download_hashtags(hashtags['output'])
-        await cl.Message("data relating to these hashtags has completed downloading.").send()
-    response = await cl.AskUserMessage("Do you want to query data related to the stories on your feed? Enter [y/n].").send()
-    if response is not None and response['output'] in "yY":
-        await download_stories()
-        await cl.Message("data relating to your feed's stories has completed downloading.").send()
-    response = await cl.AskUserMessage("Do you want to query data related to your saved posts? Enter [y/n].").send()
-    if response is not None and response['output'] in "yY":
-        await download_saved()
-        await cl.Message("data relating to your saved posts has completed downloading.").send()
     llm = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature = 0.7)
     conversation_memory = ConversationBufferMemory(memory_key="chat_history",
                                                    max_len=50,
--- extract_data.py
+++ extract_data.py
@@ -2,57 +2,36 @@ import subprocess
 import os
 import asyncio
 
-USERNAME = ""
-
-async def download_followers(username):
-    followed_directory = "followed"
-    stories_directory = "stories"
-    followed_usernames = "followed.txt"
-    USERNAME = username
-    #takes ~3 minutes right now to download all 1007 usernames followed and output to txt file
-    if not os.path.isdir(followed_directory):
-        os.mkdir(followed_directory)
-        command = f"instaloader --load-cookies chrome {username} @{username} --no-posts --no-profile-pic --no-metadata-json --dirname-pattern {followed_directory}"
-        process = await asyncio.create_subprocess_shell(command, shell = True, stdout = subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = await process.communicate()
-        with open(followed_usernames,"w") as f:
-            for file_name in os.listdir(followed_directory):
-                f.write(file_name[:-3]+"\n")
-    
-async def download_hashtags(hashtags):
-    hashtag_list = hashtags.split(",")
-    for ht in hashtag_list:
-        if not os.path.isdir(ht):
-            hashtag = ht.strip().lower()
-            command = f'instaloader --load-cookies chrome {USERNAME} "#{hashtag}" --dirname-pattern {ht}'
-            try:
+async def download_general(username):
+    directories = {
+    "followed":f"instaloader --load-cookies chrome {username} @{username} --no-posts --no-profile-pic --no-metadata-json --dirname-pattern followed",
+    "stories": f"instaloader --load-cookies chrome {username} :stories --dirname-pattern stories",
+    "saved": f"instaloader --load-cookies chrome {USERNAME} :saved --dirname-pattern saved"
+    }
+    for dir in directories:
+        if not os.path.isdir(dir):
+            os.mkdir(dir)
+            command = directories[dir]
+            if dir in ["saved"]:
+                try:
+                    process = await asyncio.create_subprocess_shell(command, shell = True, stdout = subprocess.PIPE, stderr=subprocess.PIPE)
+                    stdout, stderr = await asyncio.wait_for(process.communicate(), timeout = 30)
+                except asyncio.TimeoutError:
+                    process.kill()
+            else:
                 process = await asyncio.create_subprocess_shell(command, shell = True, stdout = subprocess.PIPE, stderr=subprocess.PIPE)
-                stdout, stderr = await asyncio.wait_for(process.communicate(), timeout = 30)
-                print("Process completed successfully.")
-            except asyncio.TimeoutError:
-                process.kill()
-                print("Process was killed.")
+                stdout, stderr = await process.communicate()
 
-async def download_stories():
-    #want metadata json right? with location data and a bunch of other extra info that can come in handy later - try to 
-    #get as much info as possible
-    stories_directory = "stories"
-    if not os.path.isdir(stories_directory):
-        os.mkdir(stories_directory)
-        command = f"instaloader --load-cookies chrome {USERNAME} :stories --dirname-pattern {stories_directory}"
-        process = await asyncio.create_subprocess_shell(command, shell = True, stdout = subprocess.PIPE, stderr=subprocess.PIPE)
-        stdout, stderr = await process.communicate()
-
-async def download_saved():
-    #need to group captions and each post's pictures together to sort..
-    saved_directory = "saved"
-    if not os.path.isdir(saved_directory):
-        os.mkdir(saved_directory)
-        command = f"instaloader --load-cookies chrome {USERNAME} :saved --dirname-pattern {saved_directory}"
-        try:
-            process = await asyncio.create_subprocess_shell(command, shell = True, stdout = subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = await asyncio.wait_for(process.communicate(), timeout = 30)
-            print("Process completed successfully.")
-        except asyncio.TimeoutError:
-            process.kill()
-            print("Process was killed.")
+# async def download_hashtags(hashtags):
+#     hashtag_list = hashtags.split(",")
+#     for ht in hashtag_list:
+#         if not os.path.isdir(ht):
+#             hashtag = ht.strip().lower()
+#             command = f'instaloader --load-cookies chrome {USERNAME} "#{hashtag}" --dirname-pattern {ht}'
+#             try:
+#                 process = await asyncio.create_subprocess_shell(command, shell = True, stdout = subprocess.PIPE, stderr=subprocess.PIPE)
+#                 stdout, stderr = await asyncio.wait_for(process.communicate(), timeout = 30)
+#                 print("Process completed successfully.")
+#             except asyncio.TimeoutError:
+#                 process.kill()
+#                 print("Process was killed.")
